c464f4358b57c36c786c8c036ada664e1e8b2636
Jesse Leite <jesseleite@gmail.com>
2024-08-19 09:44:29 -0400

    Commit crkbd 5-col keymap (wip).

---
+++ keyboards/crkbd/keymaps/jesseleite/config.h
@@ -0,0 +1,28 @@
+#pragma once
+
+// #define USE_MATRIX_I2C
+
+// Set master half
+#define MASTER_LEFT
+
+// Mod tap config
+#define TAPPING_TERM 150
+#define TAPPING_FORCE_HOLD
+#define HOLD_ON_OTHER_KEY_PRESS
+
+// #ifdef RGBLIGHT_ENABLE
+//     #define RGBLIGHT_EFFECT_BREATHING
+//     #define RGBLIGHT_EFFECT_RAINBOW_MOOD
+//     #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+//     #define RGBLIGHT_EFFECT_SNAKE
+//     #define RGBLIGHT_EFFECT_KNIGHT
+//     #define RGBLIGHT_EFFECT_CHRISTMAS
+//     #define RGBLIGHT_EFFECT_STATIC_GRADIENT
+//     #define RGBLIGHT_EFFECT_RGB_TEST
+//     #define RGBLIGHT_EFFECT_ALTERNATING
+//     #define RGBLIGHT_EFFECT_TWINKLE
+//     #define RGBLIGHT_LIMIT_VAL 120
+//     #define RGBLIGHT_HUE_STEP 10
+//     #define RGBLIGHT_SAT_STEP 17
+//     #define RGBLIGHT_VAL_STEP 17
+// #endif
