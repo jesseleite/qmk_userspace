85f27ff526e3e9b2dbfc04dcc6e8f409e8674a70
Jesse Leite <jesseleite@gmail.com>
2025-01-16 16:28:16 -0500

    Fix breaking changes.
    See: https://docs.qmk.fm/ChangeLog/20240825#key-override-keymap-c-signature-change-24120

--- keyboards/crkbd/keymaps/jesseleite/keymap.c
+++ keyboards/crkbd/keymaps/jesseleite/keymap.c
@@ -113,9 +113,8 @@ const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC
 // Register global key overrides
 // ------------------------------------------------------------------------------
 
-const key_override_t **key_overrides = (const key_override_t *[]){
+const key_override_t *key_overrides[] = {
   &delete_key_override,
-  NULL
 };
 
 
